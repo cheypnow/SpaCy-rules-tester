7aad646c9e60a3533ab6ad0c7c184d58d3369afb
Cheypnow <cheypnow@gmail.com>
2023-04-04 23:57:57 +0300

    Fix parsing

--- matcher-api/app.py
+++ matcher-api/app.py
@@ -1,15 +1,64 @@
+import logging
+import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from matcher import match_rules
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    handlers=[logging.FileHandler("app.log"), logging.StreamHandler()]
+)
 
 app = Flask(__name__)
+app.logger.handlers = logging.getLogger().handlers
 
+
+#  Rules example:
+# [
+#     {
+#         "label": "EXPERIENCE",
+#         "pattern": [
+#             [{"IS_DIGIT": true}, {"LEMMA": "год"}],
+#             [{"IS_DIGIT": true}, {"LEMMA": "year"}],
+#             [{"IS_DIGIT": true}, {"LENGTH":  1}, {"LEMMA": "year"}],
+#             [{"LEMMA": "experience"}, {"IS_PUNCT": true}]
+#         ]
+#     },
+#     {
+#         "label": "REMOTE",
+#         "pattern": [
+#             [{"LOWER": "Remote work"}]
+#         ]
+#     },
+#     {
+#         "label": "RELOCATION",
+#         "pattern": [
+#             [{"LOWER": "relocation"}]
+#         ]
+#     }
+# ]
 @app.route('/api/match', methods=['POST'])
+@cross_origin(origin='*')
 def match():
-    rules = request.json['rules']
+    rules = json.loads(request.json['rules'])
     text = request.json['text']
 
     matched_terms = match_rules(rules, text)
 
     return jsonify(matched_terms)
+
+
+@app.before_request
+def log_request_info():
+    app.logger.debug('Body: %s', request.get_data())
+
+
+@app.after_request
+def after(response):
+    app.logger.debug('Response: %s', response.get_data())
+    return response
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- matcher-api/matcher.py
+++ matcher-api/matcher.py
@@ -1,13 +1,27 @@
 import spacy
+import logging
+import json
+
 from spacy.matcher import Matcher
 
-nlp = spacy.load("en_core_web_sm")
+logger = logging.getLogger(__name__)
+
+nlp = spacy.load("en_core_web_trf")
+
 
 def match_rules(rules, text):
-
     matcher = Matcher(nlp.vocab)
+
+    logger.info(f"Rules: {rules}")
+
     for rule in rules:
-        matcher.add(rule["label"], None, rule["pattern"])
+        if isinstance(rule, dict) and "label" in rule and "pattern" in rule:
+            label = rule["label"]
+            pattern = rule["pattern"]
+            logger.info(f"Add rule {label} with patterns {pattern}")
+            matcher.add(label, pattern)
+        else:
+            logger.info(f"Invalid rule: {rule}")
 
     # Tokenize the input text using the Spacy nlp object
     doc = nlp(text)
@@ -18,6 +32,7 @@ def match_rules(rules, text):
     # Collect the matched terms and return them as a list
     matched_terms = []
     for match_id, start, end in matches:
+        logger.info(f"Match: {matches}")
         matched_terms.append(doc[start:end].text)
 
     return matched_terms
